48447d1286a98c25208c00fcffdb0bfcb95561e8
Ed Nutting <github@ednutting.com>
2020-09-20 20:46:30 +0100

    [conference]: Remove pointless `admin` field (use `Role:[confName]-admin`) instead

--- scripts/regenerateSchema.cmd
+++ scripts/regenerateSchema.cmd
@@ -5,6 +5,6 @@ move .\dump\clowdr .\schema-base
 rmdir /s /q .\dump
 popd
 
-node utils\bson2json.js -i db\schema-base -r true
-node utils\sortSchema.js -i db\schema-base\_SCHEMA.json 
-node utils\generateTSSchema.js -i db\schema-base\_SCHEMA.json -o src\classes\DataLayer\Schema
+node scripts\bson2json.js -i db\schema-base -r true
+node scripts\sortSchema.js -i db\schema-base\_SCHEMA.json 
+node scripts\generateTSSchema.js -i db\schema-base\_SCHEMA.json -o src\classes\DataLayer\Schema
--- src/classes/DataLayer/Schema/Conference.ts
+++ src/classes/DataLayer/Schema/Conference.ts
@@ -1,5 +1,5 @@
 import { Base } from ".";
-import { _User, TextChat } from "../Interface";
+import { TextChat } from "../Interface";
 import Parse from "parse";
 
 export default interface Schema extends Base {
@@ -9,6 +9,5 @@ export default interface Schema extends Base {
     shortName: string;
     welcomeText: string;
 
-    admin: Promise<_User>;
     autoSubscribeToTextChats: Promise<Array<TextChat>>;
 }
